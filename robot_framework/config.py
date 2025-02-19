fac685192e8f9c641f305eb5e274405fd90cb880
Laura-Salmonsen <balas@aarhus.dk>
2025-02-19 10:51:28 +0100

    3 attempts

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -4,7 +4,7 @@
 MAX_RETRY_COUNT = 3
 
 
-QUEUE_ATTEMPTS = 2
+QUEUE_ATTEMPTS = 3
 
 # Whether the robot should be marked as failed if MAX_RETRY_COUNT is reached.
 FAIL_ROBOT_ON_TOO_MANY_ERRORS = True
