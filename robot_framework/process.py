c4f2a342d9164403b67a0468792a241097eb5366
Laura-Salmonsen <balas@aarhus.dk>
2025-03-05 09:35:21 +0100

    With correct upload folder

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -94,7 +94,6 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
     BookmarkID = specific_content.get("Bookmark")
     OpusBookmark = orchestrator_connection.get_constant("OpusBookMarkUrl").value + str(BookmarkID)
     SharePointURL = specific_content.get("SharePointMappeLink", None)
-    SharePointURL = f'{orchestrator_connection.get_constant('AarhusKommuneSharePoint').value}/Teams/tea-teamsite11819/Delte%20dokumenter/Forms/AllItems.aspx?id=%2FTeams%2Ftea-teamsite11819%2FDelte%20dokumenter%2FOPUSrobottest&viewid=a5a48e76-9972-4980-bf37-18596d6a27be' 
     FileName = specific_content.get("Filnavn", None)
     Daily = specific_content.get("Dagligt (Ja/Nej)", None)
     MonthEnd = specific_content.get("MånedsSlut (Ja/Nej)", None)
