e19c474bffc689a8f87a367cc921fd64e44271b3
Laura-Salmonsen <balas@aarhus.dk>
2025-05-14 15:36:56 +0200

    sliced errpr message

--- robot_framework/exceptions.py
+++ robot_framework/exceptions.py
@@ -26,11 +26,12 @@ def handle_error(message: str, error: Exception, queue_element: QueueElement | N
         orchestrator_connection: A connection to OpenOrchestrator.
     """
     error_msg = f"{message}: {repr(error)}\n\nTrace:\n{traceback.format_exc()}"
+    error_msg = error_msg[:490]+error_msg[-500:]
     error_email = orchestrator_connection.get_constant(config.ERROR_EMAIL).value
 
     orchestrator_connection.log_error(error_msg)
     if queue_element:
-        orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.FAILED, error_msg)
+        orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.FAILED, error_msg[:999])
     error_screenshot.send_error_screenshot(error_email, error, orchestrator_connection.process_name)
 
 
